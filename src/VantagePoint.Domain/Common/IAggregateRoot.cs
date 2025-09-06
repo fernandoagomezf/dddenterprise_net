532b20cdb1326e6f7db08a1d5a077f1f0a2aa568
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-05 19:15:53 -0600

    Remover cosas innecesarias del agregado raíz.

--- src/VantagePoint.Domain/Common/IAggregateRoot.cs
+++ src/VantagePoint.Domain/Common/IAggregateRoot.cs
@@ -5,9 +5,5 @@ namespace VantagePoint.Domain.Common;
 
 public interface IAggregateRoot
     : IEntity {
-    IEntityCollection GetEntities();
-    IEntity GetEntity(Guid id);
-    IEntity? FindEntity(Guid id);
-    IDomainEventCollection GetEvents();
-    void Publish(DomainEvent domainEvent);
+
 }
