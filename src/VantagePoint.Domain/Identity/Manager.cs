ba43358ec35a159c1b87ee6bc779c323f4c1a5a7
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-09 21:31:08 -0600

    Refactorizar: eliminar JobInformation de las clases Manager y TeamMember

--- src/VantagePoint.Domain/Identity/Manager.cs
+++ src/VantagePoint.Domain/Identity/Manager.cs
@@ -7,15 +7,12 @@ namespace VantagePoint.Domain.Identity;
 public sealed class Manager
     : Entity {
     private readonly PersonName _name;
-    private readonly JobInformation _jobInfo;
 
-    internal Manager(Identifier id, PersonName name, JobInformation jobInfo)
+    public Manager(Identifier id, PersonName name)
         : base(id) {
         ArgumentNullException.ThrowIfNull(name);
         _name = name;
-        _jobInfo = jobInfo;
     }
 
     public PersonName Name => _name;
-    public JobInformation JobInfo => _jobInfo;
 }
