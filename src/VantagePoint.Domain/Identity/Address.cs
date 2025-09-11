822c0f89d6a06342a1fca7d9160553ed32fbd9ff
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-10 22:11:19 -0600

    refactor: eliminar la necesidad de propiedades requeridas en Address y JobInformation

--- src/VantagePoint.Domain/Identity/Address.cs
+++ src/VantagePoint.Domain/Identity/Address.cs
@@ -6,20 +6,40 @@ namespace VantagePoint.Domain.Identity;
 public sealed record Address
     : ValueObject {
     public static readonly Address Empty;
-    public required string Street { get; init; }
-    public required string City { get; init; }
-    public required string State { get; init; }
-    public required string PostalCode { get; init; }
-    public required string Country { get; init; }
+    public string Street { get; init; }
+    public string City { get; init; }
+    public string State { get; init; }
+    public string PostalCode { get; init; }
+    public string Country { get; init; }
 
     static Address() {
-        Empty = new Address {
-            Street = string.Empty,
-            City = string.Empty,
-            State = string.Empty,
-            PostalCode = string.Empty,
-            Country = string.Empty
-        };
+        Empty = new();
+    }
+
+    private Address() {
+        Street = String.Empty;
+        City = String.Empty;
+        State = String.Empty;
+        PostalCode = String.Empty;
+        Country = String.Empty;
+    }
+
+    public Address(string street)
+        : this(street, String.Empty, String.Empty, String.Empty, String.Empty) {
+
+    }
+
+    public Address(string street, string city, string state, string postalCode, string country) {
+        ArgumentNullException.ThrowIfNull(street);
+        ArgumentNullException.ThrowIfNull(city);
+        ArgumentNullException.ThrowIfNull(state);
+        ArgumentNullException.ThrowIfNull(postalCode);
+        ArgumentNullException.ThrowIfNull(country);
+        Street = street;
+        City = city;
+        State = state;
+        PostalCode = postalCode;
+        Country = country;
     }
 
     public void Deconstruct(out string street, out string city, out string state, out string postalCode, out string country) {
--- src/VantagePoint.Domain/Identity/JobInformation.cs
+++ src/VantagePoint.Domain/Identity/JobInformation.cs
@@ -6,17 +6,37 @@ namespace VantagePoint.Domain.Identity;
 public sealed record JobInformation
     : ValueObject {
     public static readonly JobInformation Empty;
-    public required string JobTitle { get; init; }
-    public required string Department { get; init; }
-    public required string Location { get; init; }
-    public required DateTime HiredDate { get; init; }
+    public string JobTitle { get; init; }
+    public string Department { get; init; }
+    public string Location { get; init; }
+    public DateTime HiredDate { get; init; }
 
     static JobInformation() {
-        Empty = new JobInformation {
-            JobTitle = String.Empty,
-            Department = String.Empty,
-            Location = String.Empty,
-            HiredDate = DateTime.MinValue
-        };
+        Empty = new();
+    }
+
+    private JobInformation() {
+        JobTitle = String.Empty;
+        Department = String.Empty;
+        Location = String.Empty;
+        HiredDate = DateTime.MinValue;
+    }
+
+    public JobInformation(string title) {
+        ArgumentNullException.ThrowIfNull(title);
+        JobTitle = title;
+        Department = String.Empty;
+        Location = String.Empty;
+        HiredDate = DateTime.MinValue;
+    }
+
+    public void Deconstruct(out string jobTitle, out string department, out string location, out DateTime hiredDate) {
+        ArgumentNullException.ThrowIfNull(JobTitle);
+        ArgumentNullException.ThrowIfNull(Department);
+        ArgumentNullException.ThrowIfNull(Location);
+        jobTitle = JobTitle;
+        department = Department;
+        location = Location;
+        hiredDate = HiredDate;
     }
 }
