4f6007538be813a7a8d509f689d4e7acb5308e41
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-10 23:08:25 -0600

    refactor: eliminar importaciones innecesarias y ajustar la interfaz IEmployeeRepository

--- src/VantagePoint.Domain/Identity/IEmployeeRepository.cs
+++ src/VantagePoint.Domain/Identity/IEmployeeRepository.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using VantagePoint.Domain.Common;
 
 namespace VantagePoint.Domain.Identity;
 
 public interface IEmployeeRepository
     : IRepository<Employee> {
-
+    IEnumerable<Employee> GetDirectReports(Identifier managerId);
 }
